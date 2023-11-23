c6ce2fb567c7fc3060c18cb03cc59e7d5af52e7e
Oscar Trujillo <98614385+oscartrugo@users.noreply.github.com>
2023-11-23 14:17:39 -0600

    210. Creando la clase ejemplo con el método main

--- poo_interfaces/src/org/oscartrugo/poointerfaces/imprenta/EjemploImprenta.java
+++ poo_interfaces/src/org/oscartrugo/poointerfaces/imprenta/EjemploImprenta.java
@@ -1,7 +1,23 @@
 package org.oscartrugo.poointerfaces.imprenta;
 
+import org.oscartrugo.poointerfaces.imprenta.modelo.*;
+
 public class EjemploImprenta {
     public static void main(String[] args) {
+        Curriculo cv = new Curriculo("Resumen laboral...", "Oscar Trujillo", "Ing. en Sistemas Computacionales");
+        cv.addExperiencia("Java");
+        cv.addExperiencia("JavaScript");
+        cv.addExperiencia("Postgres");
+        cv.addExperiencia("Spring Boot");
+        cv.addExperiencia("TypScript");
 
+        Informe informe = new Informe("Estudio sobre microservicios", "Oscar Trujillo", "Martin Flower");
+
+        imprimir(cv);
+        imprimir(informe);
+    }
+
+    public static void imprimir(Hoja imprimible){
+        System.out.println(imprimible.imprimir());
     }
 }

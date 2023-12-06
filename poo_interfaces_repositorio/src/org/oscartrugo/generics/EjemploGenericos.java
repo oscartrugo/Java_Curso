4818f148a94e701138a48d4928ebb38971715db8
Oscar Trujillo <98614385+oscartrugo@users.noreply.github.com>
2023-12-06 13:04:11 -0600

    222. Métodos genéricos

---
+++ poo_interfaces_repositorio/src/org/oscartrugo/generics/EjemploGenericos.java
@@ -0,0 +1,43 @@
+package org.oscartrugo.generics;
+
+import org.oscartrugo.poointerfaces.modelo.Cliente;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class EjemploGenericos {
+    public static void main(String[] args) {
+
+        List<Cliente> clientes = new ArrayList<>();
+        clientes.add(new Cliente("Oscar", "Trujillo"));
+
+        Cliente oscar = clientes.iterator().next();
+
+        Cliente[] clientesArreglo = {new Cliente("Luci", "Martinez"),
+                new Cliente("Oscar", "Trujillo")};
+
+        Integer[] enterosArreglo = {1, 2, 3};
+
+        List<Cliente> clientesLista = fromArrayToList(clientesArreglo);
+        List<Integer> enterosLista = fromArrayToList(enterosArreglo);
+
+        clientesLista.forEach(System.out::println);
+        enterosLista.forEach(System.out::println);
+
+        List<String> nombres = fromArrayToList(new String[]{"Oscar", "Pepe", "Luci", "Bea", "John"},
+                enterosArreglo);
+        nombres.forEach(System.out::println);
+    }
+
+    public static <T> List<T> fromArrayToList(T[] clientes){ //Convierte arreglo a  lista
+        return Arrays.asList(clientes);
+    }
+
+    public static <T, G> List<T> fromArrayToList(T[] c, G[] x){ //Convierte arreglo a  lista
+        for (G elemento: x){
+            System.out.println(elemento);
+        }
+        return Arrays.asList(c);
+    }
+}

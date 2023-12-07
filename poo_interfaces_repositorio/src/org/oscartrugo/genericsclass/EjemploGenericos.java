2cfa5e8997e0e5ec51e418014f894f5269345858
Oscar Trujillo <98614385+oscartrugo@users.noreply.github.com>
2023-12-06 19:17:48 -0600

    228. Clases genéricas - modificando la clase genérica con el parámetro T

--- poo_interfaces_repositorio/src/org/oscartrugo/genericsclass/EjemploGenericos.java
+++ poo_interfaces_repositorio/src/org/oscartrugo/genericsclass/EjemploGenericos.java
@@ -1,38 +1,42 @@
 package org.oscartrugo.genericsclass;
 
 public class EjemploGenericos {
+
+    public static <T> void imprimirCamion(Camion<T> camion){
+        for(T a: camion){
+            if(a instanceof Animal){
+                System.out.println(((Animal)a).getNombre() + " tipo: " + ((Animal)a).getTipo());
+            }else if(a instanceof Automovil){
+                System.out.println(((Automovil)a).getMarca());
+            }else if(a instanceof Maquinaria){
+                System.out.println(((Maquinaria)a).getTipo());
+            }
+        }
+    }
+
     public static void main(String[] args) {
 
-        Camion transporteCaballos = new Camion(5);
+        Camion<Animal> transporteCaballos = new Camion<>(5); //Camión de animales
         transporteCaballos.add(new Animal("Peregrino", "Caballo"));
         transporteCaballos.add(new Animal("Grillo", "Caballo"));
         transporteCaballos.add(new Animal("Tunquen", "Caballo"));
         transporteCaballos.add(new Animal("Topocalma", "Caballo"));
         transporteCaballos.add(new Animal("Longotoma", "Caballo"));
 
-        for(Object objeto: transporteCaballos){
-            Animal animal = (Animal) objeto;
-            System.out.println(animal.getNombre() + " tipo: " + animal.getTipo());
-        }
+        imprimirCamion(transporteCaballos);
 
-        Camion transMaquinas = new Camion(3);
+        Camion<Maquinaria> transMaquinas = new Camion<>(3);
         transMaquinas.add(new Maquinaria("Bulldozer"));
         transMaquinas.add(new Maquinaria("Grua Horquilla"));
         transMaquinas.add(new Maquinaria("Perforadora"));
 
-        for (Object objeto: transMaquinas){
-            Maquinaria maquinaria = (Maquinaria) objeto;
-            System.out.println(maquinaria.getTipo());
-        }
+        imprimirCamion(transMaquinas);
 
-        Camion transAuto = new Camion(3);
+        Camion<Automovil> transAuto = new Camion<>(3);
         transAuto.add(new Automovil("Toyota"));
         transAuto.add(new Automovil("Mitsubishi"));
         transAuto.add(new Automovil("Chevrolet"));
 
-        for (Object objeto: transAuto){
-            Automovil automovil = (Automovil) objeto;
-            System.out.println(automovil.getMarca());
-        }
+        imprimirCamion(transAuto);
     }
 }

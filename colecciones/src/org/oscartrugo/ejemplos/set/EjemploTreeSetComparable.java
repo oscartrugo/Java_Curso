b8e610b237c92db47c8fa7d6063398e432bb5e51
Oscar Trujillo <98614385+oscartrugo@users.noreply.github.com>
2024-02-21 14:23:27 -0600

    244. Ejemplo TreeSet Comparable y Comparator

---
+++ colecciones/src/org/oscartrugo/ejemplos/set/EjemploTreeSetComparable.java
@@ -0,0 +1,21 @@
+package org.oscartrugo.ejemplos.set;
+
+import org.oscartrugo.ejemplos.modelo.Alumno;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class EjemploTreeSetComparable {
+    public static void main(String[] args) {
+        Set<Alumno> sa = new TreeSet<>((a, b) -> b.getNota().compareTo(a.getNota())); //Sobreescribe la comparación por nota
+        sa.add(new Alumno("Pato", 5));
+        sa.add(new Alumno("Cata", 6));
+        sa.add(new Alumno("Lucy", 4));
+        sa.add(new Alumno("Jano", 7));
+        sa.add(new Alumno("Andres", 3));
+        sa.add(new Alumno("Zeus", 2));
+
+        System.out.println(sa);
+
+    }
+}

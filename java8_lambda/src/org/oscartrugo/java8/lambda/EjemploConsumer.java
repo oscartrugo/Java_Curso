9723a4f65579d87f90526ce5e2b5356d89de5a53
Oscar Trujillo <98614385+oscartrugo@users.noreply.github.com>
2024-04-16 21:58:15 -0600

    276. Referencias de métodos en expresiones lambda de tipo Consumer

--- java8_lambda/src/org/oscartrugo/java8/lambda/EjemploConsumer.java
+++ java8_lambda/src/org/oscartrugo/java8/lambda/EjemploConsumer.java
@@ -1,9 +1,14 @@
 package org.oscartrugo.java8.lambda;
 
+import org.oscartrugo.java8.lambda.models.Usuario;
+
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 public class EjemploConsumer {
 
@@ -14,11 +19,27 @@ public class EjemploConsumer {
         };
         consumidor.accept(new Date());
 
-        BiConsumer<String, Integer> consumidorBi = (nombre, edad) -> {
-            System.out.println(nombre + ", tiene " + edad + " años!");
-        };
+        BiConsumer<String, Integer> consumidorBi = (nombre, edad) -> System.out.println(nombre + ", tiene " + edad + " años!");
 
         consumidorBi.accept("Oscar", 25);
+
+        Consumer<String> consumidor2 = System.out::println;
+        consumidor2.accept("Hola Mundo");
+
+        List<String> nombres = Arrays.asList("Oscar", "Pepe", "Luz", "Paco");
+        nombres.forEach(consumidor2);
+
+        Supplier<Usuario> crearUsuario = Usuario::new;
+        Usuario usuario = crearUsuario.get();
+
+        BiConsumer<Usuario, String> asignarNombre = Usuario::setNombre;
+
+        asignarNombre.accept(usuario, "Oscar");
+        System.out.println("Nombre usuario: " + usuario.getNombre());
+
+        Supplier<String> proveedor = () ->  "Hola mundo lambda supplier";
+
+        System.out.println(proveedor.get());
     }
 
 }

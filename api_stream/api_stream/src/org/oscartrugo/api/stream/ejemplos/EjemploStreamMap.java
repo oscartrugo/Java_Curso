0a82220a484d5cb2e4ae94865027eefae7726e07
Oscar Trujillo <98614385+oscartrugo@users.noreply.github.com>
2024-05-19 14:14:35 -0600

    283. El operador map

---
+++ api_stream/api_stream/src/org/oscartrugo/api/stream/ejemplos/EjemploStreamMap.java
@@ -0,0 +1,20 @@
+package org.oscartrugo.api.stream.ejemplos;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class EjemploStreamMap {
+    public static void main(String[] args) {
+
+        Stream<String> nombres = Stream
+                .of("Pato", "Paco", "Pepa", "Pepe")
+                .map(String::toUpperCase)
+                .peek(System.out::println)
+                .map(String::toLowerCase);
+
+        List<String> lista = nombres.collect(Collectors.toList());
+        lista.forEach(System.out::println);
+    }
+}

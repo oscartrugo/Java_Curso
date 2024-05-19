085906ad60d79d41158535cf3a935b4dfe04fb9d
Oscar Trujillo <98614385+oscartrugo@users.noreply.github.com>
2024-05-19 15:59:43 -0600

    286. El operador anyMatch

---
+++ api_stream/api_stream/src/org/oscartrugo/api/stream/ejemplos/EjemploStreamFilterCount.java
@@ -0,0 +1,18 @@
+package org.oscartrugo.api.stream.ejemplos;
+
+import org.oscartrugo.api.stream.ejemplos.models.Usuario;
+
+import java.util.stream.Stream;
+
+public class EjemploStreamFilterCount {
+    public static void main(String[] args) {
+
+        long count = Stream
+                .of("Pato Guzman", "Paco Gonzalez", "Pepa Gutierrez", "Pepe Mena", "Pepe Garcia")
+                .map(nombre -> new Usuario(nombre.split(" ")[0], nombre.split(" ")[1]))
+                .peek(System.out::println)
+                .count();
+
+        System.out.println(count);
+    }
+}

c391f05453aee94218704290234b47ec5f096aca
Oscar Trujillo <98614385+oscartrugo@users.noreply.github.com>
2024-09-09 00:31:50 -0600

    290. Operador flatMap()

--- api_stream/api_stream/src/org/oscartrugo/api/stream/ejemplos/EjemploStreamFilter.java
+++ api_stream/api_stream/src/org/oscartrugo/api/stream/ejemplos/EjemploStreamFilter.java
@@ -10,13 +10,18 @@ public class EjemploStreamFilter {
     public static void main(String[] args) {
 
         Stream<Usuario> nombres = Stream
-                .of("Pato Guzman", "Paco Gonzalez", "Pepa Gutierrez", "Pepe Mena", "Pepe Garcia")
+                .of("Pato Guzman", "Paco Gonzalez", "Pepa Gutierrez", "Pepe Mena",
+                        "Pepe Garcia")
                 .map(nombre -> new Usuario(nombre.split(" ")[0], nombre.split(" ")[1]))
-                .filter(u -> u.getNombre().equals("Pepe"))
+                .flatMap(u -> {
+                    if(u.getNombre().equalsIgnoreCase("Pepe")){
+                        return Stream.of(u);
+                    }
+                    return Stream.empty();
+                })
                 .peek(System.out::println);
 
-        List<Usuario> lista = nombres.collect(Collectors.toList());
-        lista.forEach(System.out::println);
-//        lista.forEach(System.out::println);
+        //nombres.forEach(System.out::println);
+        System.out.println(nombres.count());
     }
 }
